4cf91ced38e6cb23827e128b565189eda0f10076
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-04-02 02:02:17 -0500

    Indentation issue?

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 apt-get update
 apt-get upgrade -y
 apt-get install curl -y
 echo "tools installed"
+
 response=$(curl --silent --show-error --fail -X GET "127.0.0.1/testing/run/basic")
-if [$response == 200];
-then
-exit 0
+if [$response == 200]; then
+  exit 0
 fi
 exit 1
