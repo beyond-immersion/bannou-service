3a99ea4ff73c5206663761309a8efad2ecca9d20
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-04-02 02:06:21 -0500

    Spacing fix

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -6,7 +6,8 @@ apt-get install curl -y
 echo "tools installed"
 
 response=$(curl --silent --show-error --fail -X GET "127.0.0.1/testing/run/basic")
-if [$response == 200]; then
-  exit
+if [ $response == 200 ]
+then
+  exit 0
 fi
 exit 1
