f1266f77ece08139d463242b82b9dcb3328af99b
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-04-01 23:12:52 -0500

    Try localhost

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -1,5 +1,5 @@
 docker exec -it provisioning_bannou_1 bash
-response=$(curl --write-out '%{http_code}' --output /dev/null '127.0.0.1:80/testing/run/basic')
+response=$(curl --write-out '%{http_code}' --output /dev/null 'localhost/testing/run/basic')
 exit
 echo "API response:" response
 if [ response == 200 ]; then
