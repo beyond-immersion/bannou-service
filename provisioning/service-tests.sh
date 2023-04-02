ab2371e84f0272f8cb2bbfb0e62c1fd6cde64738
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-04-02 02:16:22 -0500

    Try use unary operator

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -6,7 +6,7 @@ apt-get install curl -y
 echo "tools installed"
 
 response=$(curl --silent --show-error --fail -X GET "127.0.0.1/testing/run/basic")
-if [ $response == 200 ]
+if [ -z "$response" ]
 then
   exit 0
 fi
