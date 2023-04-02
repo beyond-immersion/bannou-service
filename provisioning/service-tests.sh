99ccacd12ec7be0fb14fe8414a8bba99b58d41fe
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-04-01 23:08:56 -0500

    Comment out tests to iterate faster

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -1,5 +1,5 @@
-docker exec -i provisioning_bannou_1 bash
-response=$(curl --write-out '%{http_code}' --silent --output /dev/null '127.0.0.1:80/testing/run/basic')
+docker exec -it provisioning_bannou_1 bash
+response=$(curl --write-out '%{http_code}' --output /dev/null '127.0.0.1:80/testing/run/basic')
 exit
 echo "API response:" response
 if [ response == 200 ]; then
