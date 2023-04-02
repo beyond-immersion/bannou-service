0f988ef28beeb82e096e04cac06b459e06929cf8
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-04-01 22:43:17 -0500

    Update shell script

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -1,4 +1,8 @@
-docker exec -i cl-bannou-1 bash
-response=$(curl --write-out '%{http_code}' --silent --output /dev/null 'http://127.0.0.1/testing/run')
+docker exec -i provisioning-bannou-1 bash
+response=$(curl --write-out '%{http_code}' --silent --output /dev/null 'http://127.0.0.1/testing/run/basic')
 exit
-echo response
+echo "API response:" response
+if [ response == 200 ]; then
+exit
+fi
+exit 1
