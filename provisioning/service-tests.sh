b74f3b31a2e70f31db4624e9b1c30ed2fcdc47d3
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-04-02 01:39:34 -0500

    Ensure newlines don't change in shell script

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -1,1 +1,11 @@
-docker exec -i cl-bannou-1 bash -c curl 127.0.0.1/testing/run/basic
+#!/bin/bash
+apt-get update
+apt-get upgrade -y
+apt-get install curl -y
+echo "tools installed"
+response=$(curl --silent --show-error --fail -X GET "127.0.0.1/testing/run/basic")
+if [response == 200];
+then
+exit 0
+fi
+exit 1
