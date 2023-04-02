98e266ff77a5e34415ef3df00ead9f58660cad94
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-04-01 22:47:46 -0500

    Try again

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -1,8 +1,8 @@
-docker exec -i provisioning-bannou-1 bash
+docker exec -i provisioning_bannou_1 bash
 response=$(curl --write-out '%{http_code}' --silent --output /dev/null 'http://127.0.0.1/testing/run/basic')
 exit
 echo "API response:" response
 if [ response == 200 ]; then
-exit
+    exit
 fi
 exit 1
