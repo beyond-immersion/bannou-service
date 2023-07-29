1b41e8fe2542535d77f449c2941f4b7fe871ce56
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-07-29 14:51:12 -0500

    Try passthru ENVs to integration test server

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -2,10 +2,9 @@
 
 apt-get update
 apt-get install curl -y
-apt-get install jq -y
 echo "tools installed"
 
 if curl --fail -X GET "127.0.0.1/testing/run-enabled"; then
-  exit 1
+  exit 0
 fi
-exit 0
+exit 1
