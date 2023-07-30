6157ecdf2162178ce31b3c0c3f9c77a3609bd98c
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-07-30 14:25:44 -0500

    Move ENVs from workflow to CI compose- add comments

--- bannou-service/Services/Configuration/AuthorizationServiceConfiguration.cs
+++ bannou-service/Services/Configuration/AuthorizationServiceConfiguration.cs
@@ -7,6 +7,7 @@ namespace BeyondImmersion.BannouService.Services.Configuration;
 [ServiceConfiguration(serviceType: typeof(AuthorizationService), envPrefix: "AUTHORIZATION_", primary: true)]
 public class AuthorizationServiceConfiguration : ServiceConfiguration
 {
+    public bool Testing { get; set; }
     public string Token_Public_Key { get; set; }
     public string Token_Private_Key { get; set; }
 }
