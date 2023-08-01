157e61d9a53f5867c5c59ac96fb05b7ed397e99d
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-07-31 21:14:30 -0500

    Run curls insecure- self-signed certs are fine for CI testing/healthchecks

--- provisioning/service-tests.sh
+++ provisioning/service-tests.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install curl -y
 echo "tools installed"
 
-if curl --fail -X GET "https://127.0.0.1:80/testing/run-enabled"; then
+if curl -k --fail -X GET "https://127.0.0.1:80/testing/run-enabled"; then
   exit 0
 fi
 exit 1
