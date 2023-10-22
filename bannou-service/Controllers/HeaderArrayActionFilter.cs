64762e4da33d7c4f5206f79796e30d57ef720927
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-10-22 07:59:29 -0400

    Fill in remaining logic for action filter

--- bannou-service/Controllers/HeaderArrayActionFilter.cs
+++ bannou-service/Controllers/HeaderArrayActionFilter.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System.Reflection;
 
 namespace BeyondImmersion.BannouService.Controllers;
@@ -17,21 +17,87 @@ public class HeaderArrayActionFilter : IActionFilter
                 var headerAttr = property.GetCustomAttribute<ToHeaderArrayAttribute>();
                 if (headerAttr != null)
                 {
-                    if (property.PropertyType.IsAssignableFrom(typeof(Dictionary<string, string[]>)))
-                    {
-
-                    }
-
-                    var value = property.GetValue(objectResult.Value)?.ToString();
-                    if (value != null)
-                        context.HttpContext.Response.Headers[property.Name] = value;
-
+                    SetPropertyValuesToHeaderArray(context, objectResult, property, headerAttr);
                     propertiesToRemove.Add(property.Name);
                 }
             }
 
             foreach (var propName in propertiesToRemove)
                 objectResult.Value.GetType().GetProperty(propName)?.SetValue(objectResult.Value, null);
+        }
+    }
+
+    private static void SetPropertyValuesToHeaderArray(ActionExecutedContext context, ObjectResult objectResult, PropertyInfo propertyInfo, ToHeaderArrayAttribute headerAttr)
+    {
+        try
+        {
+            var propType = propertyInfo.PropertyType;
+            var propVal = propertyInfo.GetValue(objectResult.Value);
+            if (propVal == null)
+                return;
+
+            var headerKVPs = new List<(string, string?)>();
+            switch (propVal)
+            {
+                case IEnumerable<KeyValuePair<string, string[]>> realPropVal:
+                    foreach (var kvp in realPropVal)
+                        foreach (var stringVal in kvp.Value)
+                            headerKVPs.Add((kvp.Key, stringVal));
+
+                    break;
+
+                case IEnumerable<KeyValuePair<string, List<string>>> realPropVal:
+                    foreach (var kvp in realPropVal)
+                        foreach (var stringVal in kvp.Value)
+                            headerKVPs.Add((kvp.Key, stringVal));
+
+                    break;
+
+                case IEnumerable<KeyValuePair<string, string>> realPropVal:
+                    foreach (var kvp in realPropVal)
+                        headerKVPs.Add((kvp.Key, kvp.Value));
+
+                    break;
+
+                case IEnumerable<(string, string)> realPropVal:
+                    foreach (var kvp in realPropVal)
+                        headerKVPs.Add((kvp.Item1, kvp.Item2));
+
+                    break;
+
+                case IEnumerable<string> realPropVal:
+                    foreach (var stringVal in realPropVal)
+                        headerKVPs.Add((stringVal, null));
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (headerKVPs.Count == 0)
+                return;
+
+            var delin = "__";
+            if (!string.IsNullOrWhiteSpace(headerAttr.Delineator))
+                delin = headerAttr.Delineator;
+
+            var headerName = headerAttr.Name ?? propertyInfo.Name;
+            if (headerKVPs.Count > 1 && !headerName.EndsWith("[]"))
+                headerName += "[]";
+
+            foreach (var headerKVP in headerKVPs)
+            {
+                var headerValue = headerKVP.Item1;
+                if (!string.IsNullOrWhiteSpace(headerKVP.Item2))
+                    headerValue = headerValue + delin + headerKVP.Item2;
+
+                context.HttpContext.Response.Headers[headerName] = headerValue;
+            }
+        }
+        catch (Exception exc)
+        {
+            Program.Logger.Log(LogLevel.Error, exc, $"Could not set property values for '{propertyInfo.Name}' of type '{propertyInfo.PropertyType.Name}' to header array.");
         }
     }
 }
