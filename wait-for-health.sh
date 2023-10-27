d9b1776f0f24a5cc4d435a4b1761c149df8c3155
Donald Maxwell <donaldjmaxwell@gmail.com>
2023-10-27 19:51:33 -0400

    Try something

--- wait-for-health.sh
+++ wait-for-health.sh
@@ -15,7 +15,6 @@ COUNT=0
 while [[ $COUNT -lt $MAX_RETRIES ]]
 do
     RESPONSE_CODE=$(curl --connect-timeout 5 -o /dev/null -k -s -w "%{http_code}" $ENDPOINT 2>/dev/null)
-    echo "Curl response: $RESPONSE_CODE"
 
     if [[ $RESPONSE_CODE -eq 200 ]]; then
         echo "Service is ready!"
