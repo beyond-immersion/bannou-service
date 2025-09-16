6e183cd5049dffc56d17d6e885a8d653704d36a7
DonLoquacious <donaldjmaxwell@gmail.com>
2025-09-16 17:23:12 -0400

    Line endings

--- lib-accounts/Generated/AccountsClient.cs
+++ lib-accounts/Generated/AccountsClient.cs
@@ -1463,7 +1463,7 @@ public partial class AccountsClient : BeyondImmersion.BannouService.ServiceClien
                 var field = System.Reflection.IntrospectionExtensions.GetTypeInfo(value.GetType()).GetDeclaredField(name);
                 if (field != null)
                 {
-                    var attribute = System.Reflection.CustomAttributeExtensions.GetCustomAttribute(field, typeof(System.Runtime.Serialization.EnumMemberAttribute)) 
+                    var attribute = System.Reflection.CustomAttributeExtensions.GetCustomAttribute(field, typeof(System.Runtime.Serialization.EnumMemberAttribute))
                         as System.Runtime.Serialization.EnumMemberAttribute;
                     if (attribute != null)
                     {
@@ -1475,7 +1475,7 @@ public partial class AccountsClient : BeyondImmersion.BannouService.ServiceClien
                 return converted == null ? string.Empty : converted;
             }
         }
-        else if (value is bool) 
+        else if (value is bool)
         {
             return System.Convert.ToString((bool)value, cultureInfo).ToLowerInvariant();
         }
--- lib-behavior/Generated/IBehaviorService.cs
+++ lib-behavior/Generated/IBehaviorService.cs
@@ -9,34 +9,34 @@ namespace BeyondImmersion.BannouService.Behavior;
 /// </summary>
 public interface IBehaviorService
 {
-        /// <summary>
-        /// CompileAbmlBehavior operation
-        /// </summary>
-        Task<(StatusCodes, CompileBehaviorResponse?)> CompileAbmlBehaviorAsync(CompileBehaviorRequest body, CancellationToken cancellationToken = default(CancellationToken));
+    /// <summary>
+    /// CompileAbmlBehavior operation
+    /// </summary>
+    Task<(StatusCodes, CompileBehaviorResponse?)> CompileAbmlBehaviorAsync(CompileBehaviorRequest body, CancellationToken cancellationToken = default(CancellationToken));
 
-        /// <summary>
-        /// CompileBehaviorStack operation
-        /// </summary>
-        Task<(StatusCodes, CompileBehaviorResponse?)> CompileBehaviorStackAsync(BehaviorStackRequest body, CancellationToken cancellationToken = default(CancellationToken));
+    /// <summary>
+    /// CompileBehaviorStack operation
+    /// </summary>
+    Task<(StatusCodes, CompileBehaviorResponse?)> CompileBehaviorStackAsync(BehaviorStackRequest body, CancellationToken cancellationToken = default(CancellationToken));
 
-        /// <summary>
-        /// ValidateAbml operation
-        /// </summary>
-        Task<(StatusCodes, ValidateAbmlResponse?)> ValidateAbmlAsync(ValidateAbmlRequest body, CancellationToken cancellationToken = default(CancellationToken));
+    /// <summary>
+    /// ValidateAbml operation
+    /// </summary>
+    Task<(StatusCodes, ValidateAbmlResponse?)> ValidateAbmlAsync(ValidateAbmlRequest body, CancellationToken cancellationToken = default(CancellationToken));
 
-        /// <summary>
-        /// GetCachedBehavior operation
-        /// </summary>
-        Task<(StatusCodes, CachedBehaviorResponse?)> GetCachedBehaviorAsync(string behavior_id, CancellationToken cancellationToken = default(CancellationToken));
+    /// <summary>
+    /// GetCachedBehavior operation
+    /// </summary>
+    Task<(StatusCodes, CachedBehaviorResponse?)> GetCachedBehaviorAsync(string behavior_id, CancellationToken cancellationToken = default(CancellationToken));
 
-        /// <summary>
-        /// InvalidateCachedBehavior operation
-        /// </summary>
-        Task<(StatusCodes, object?)> InvalidateCachedBehaviorAsync(string behavior_id, CancellationToken cancellationToken = default(CancellationToken));
+    /// <summary>
+    /// InvalidateCachedBehavior operation
+    /// </summary>
+    Task<(StatusCodes, object?)> InvalidateCachedBehaviorAsync(string behavior_id, CancellationToken cancellationToken = default(CancellationToken));
 
-        /// <summary>
-        /// ResolveContextVariables operation
-        /// </summary>
-        Task<(StatusCodes, ResolveContextResponse?)> ResolveContextVariablesAsync(ResolveContextRequest body, CancellationToken cancellationToken = default(CancellationToken));
+    /// <summary>
+    /// ResolveContextVariables operation
+    /// </summary>
+    Task<(StatusCodes, ResolveContextResponse?)> ResolveContextVariablesAsync(ResolveContextRequest body, CancellationToken cancellationToken = default(CancellationToken));
 
 }
