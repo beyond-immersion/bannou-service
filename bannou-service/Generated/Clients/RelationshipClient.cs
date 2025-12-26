7626c190ceff31026bde050e3c51d3fba9489e3a
DonLoquacious <donaldjmaxwell@gmail.com>
2025-12-26 02:50:36 -0500

    Cleanup

--- bannou-service/Generated/Clients/RelationshipClient.cs
+++ bannou-service/Generated/Clients/RelationshipClient.cs
@@ -928,7 +928,7 @@ public partial class RelationshipClient : IRelationshipClient, BeyondImmersion.B
                 var field = System.Reflection.IntrospectionExtensions.GetTypeInfo(value.GetType()).GetDeclaredField(name);
                 if (field != null)
                 {
-                    var attribute = System.Reflection.CustomAttributeExtensions.GetCustomAttribute(field, typeof(System.Runtime.Serialization.EnumMemberAttribute)) 
+                    var attribute = System.Reflection.CustomAttributeExtensions.GetCustomAttribute(field, typeof(System.Runtime.Serialization.EnumMemberAttribute))
                         as System.Runtime.Serialization.EnumMemberAttribute;
                     if (attribute != null)
                     {
@@ -940,7 +940,7 @@ public partial class RelationshipClient : IRelationshipClient, BeyondImmersion.B
                 return converted == null ? string.Empty : converted;
             }
         }
-        else if (value is bool) 
+        else if (value is bool)
         {
             return System.Convert.ToString((bool)value, cultureInfo).ToLowerInvariant();
         }
