ec739ef9f8c1777dc7cf2a714dbef57fdfa41d8b
DonLoquacious <donaldjmaxwell@gmail.com>
2026-02-04 21:45:51 -0500

    Sanity

--- bannou-service/Generated/Models/RealmHistoryModels.cs
+++ bannou-service/Generated/Models/RealmHistoryModels.cs
@@ -25,6 +25,21 @@
 using BeyondImmersion.BannouService;
 using BeyondImmersion.BannouService.RealmHistory;
 
+#pragma warning disable 108 // Disable "CS0108 '{derivedDto}.ToJson()' hides inherited member '{dtoBase}.ToJson()'. Use the new keyword if hiding was intended."
+#pragma warning disable 114 // Disable "CS0114 '{derivedDto}.RaisePropertyChanged(String)' hides inherited member 'dtoBase.RaisePropertyChanged(String)'. To make the current member override that implementation, add the override keyword. Otherwise add the new keyword."
+#pragma warning disable 472 // Disable "CS0472 The result of the expression is always 'false' since a value of type 'Int32' is never equal to 'null' of type 'Int32?'
+#pragma warning disable 612 // Disable "CS0612 '...' is obsolete"
+#pragma warning disable 649 // Disable "CS0649 Field is never assigned to, and will always have its default value null"
+#pragma warning disable 1573 // Disable "CS1573 Parameter '...' has no matching param tag in the XML comment for ...
+#pragma warning disable 1591 // Disable "CS1591 Missing XML comment for publicly visible type or member ..."
+#pragma warning disable 8073 // Disable "CS8073 The result of the expression is always 'false' since a value of type 'T' is never equal to 'null' of type 'T?'"
+#pragma warning disable 3016 // Disable "CS3016 Arrays as attribute arguments is not CLS-compliant"
+#pragma warning disable 8600 // Disable "CS8600 Converting null literal or possible null value to non-nullable type"
+#pragma warning disable 8602 // Disable "CS8602 Dereference of a possibly null reference"
+#pragma warning disable 8603 // Disable "CS8603 Possible null reference return"
+#pragma warning disable 8604 // Disable "CS8604 Possible null reference argument for parameter"
+#pragma warning disable 8625 // Disable "CS8625 Cannot convert null literal to non-nullable reference type"
+#pragma warning disable 8765 // Disable "CS8765 Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes)."
 
 namespace BeyondImmersion.BannouService.RealmHistory;
 
@@ -766,6 +781,139 @@ public partial class RealmHistorySummaryResponse
     [System.ComponentModel.DataAnnotations.Required]
     [System.Text.Json.Serialization.JsonRequired]
     public System.Collections.Generic.ICollection<string> MajorHistoricalEvents { get; set; } = new System.Collections.ObjectModel.Collection<string>();
+
+}
+
+/// <summary>
+/// Request to get realm history data for compression
+/// </summary>
+[System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.5.0.0 (NJsonSchema v11.4.0.0 (Newtonsoft.Json v13.0.0.0))")]
+public partial class GetCompressDataRequest
+{
+
+    /// <summary>
+    /// ID of the realm to get compress data for
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("realmId")]
+    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+    [System.Text.Json.Serialization.JsonRequired]
+    public System.Guid RealmId { get; set; } = default!;
+
+}
+
+/// <summary>
+/// Compressed realm lore and history for archive storage and storyline SDK consumption.
+/// <br/>Inherits base archive properties from ResourceArchiveBase.
+/// <br/>The realmId field equals resourceId for convenience.
+/// <br/>
+/// </summary>
+[System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.5.0.0 (NJsonSchema v11.4.0.0 (Newtonsoft.Json v13.0.0.0))")]
+public partial class RealmHistoryArchive : ResourceArchiveBase
+{
+
+    /// <summary>
+    /// Realm this data belongs to (equals resourceId)
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("realmId")]
+    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+    [System.Text.Json.Serialization.JsonRequired]
+    public System.Guid RealmId { get; set; } = default!;
+
+    /// <summary>
+    /// Whether lore elements exist
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("hasLore")]
+    public bool HasLore { get; set; } = default!;
+
+    /// <summary>
+    /// Lore elements (empty if hasLore=false)
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("loreElements")]
+    public System.Collections.Generic.ICollection<RealmLoreResponse> LoreElements { get; set; } = default!;
+
+    /// <summary>
+    /// Whether event participations exist
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("hasParticipations")]
+    public bool HasParticipations { get; set; } = default!;
+
+    /// <summary>
+    /// Event participations (empty if hasParticipations=false)
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("participations")]
+    public System.Collections.Generic.ICollection<RealmHistoricalParticipation> Participations { get; set; } = default!;
+
+    /// <summary>
+    /// Text summaries for reference
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("summaries")]
+    public RealmHistorySummaryResponse? Summaries { get; set; } = default!;
+
+}
+
+/// <summary>
+/// Request to restore realm history data from archive
+/// </summary>
+[System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.5.0.0 (NJsonSchema v11.4.0.0 (Newtonsoft.Json v13.0.0.0))")]
+public partial class RestoreFromArchiveRequest
+{
+
+    /// <summary>
+    /// ID of the realm to restore data for
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("realmId")]
+    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+    [System.Text.Json.Serialization.JsonRequired]
+    public System.Guid RealmId { get; set; } = default!;
+
+    /// <summary>
+    /// Base64-encoded gzipped RealmHistoryArchive JSON
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("data")]
+    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+    [System.Text.Json.Serialization.JsonRequired]
+    public string Data { get; set; } = default!;
+
+}
+
+/// <summary>
+/// Result of restoration from archive
+/// </summary>
+[System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.5.0.0 (NJsonSchema v11.4.0.0 (Newtonsoft.Json v13.0.0.0))")]
+public partial class RestoreFromArchiveResponse
+{
+
+    /// <summary>
+    /// Realm data was restored for
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("realmId")]
+    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+    [System.Text.Json.Serialization.JsonRequired]
+    public System.Guid RealmId { get; set; } = default!;
+
+    /// <summary>
+    /// Number of lore elements restored
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("loreRestored")]
+    public int LoreRestored { get; set; } = default!;
+
+    /// <summary>
+    /// Number of participation records restored
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("participationsRestored")]
+    public int ParticipationsRestored { get; set; } = default!;
+
+    /// <summary>
+    /// Whether restoration completed successfully
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("success")]
+    public bool Success { get; set; } = default!;
+
+    /// <summary>
+    /// Error details if restoration failed
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("errorMessage")]
+    public string? ErrorMessage { get; set; } = default!;
 
 }
 
